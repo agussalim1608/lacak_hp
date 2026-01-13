0610b04c0c8cc5e57e400566e0df02a1f03dbd8f
Agus Salim <agus.salim1608@gmail.com>
2026-01-13 19:03:31 +0700

    fix public folder

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const crypto = require("crypto");
 
 app.use(express.json());
 app.use(express.static("public"));
 
-app.post("/save-location", (req,res)=>{
-  fs.writeFileSync("database.json", JSON.stringify(req.body));
-  res.send({status:"saved"});
+const PASSWORD = "agus123"; // ganti dengan password kamu
+
+let data = {};
+
+app.post("/login",(req,res)=>{
+ if(req.body.password === PASSWORD) res.send({ok:true});
+ else res.send({ok:false});
 });
 
-app.get("/location", (req,res)=>{
-  if(fs.existsSync("database.json")){
-     res.send(fs.readFileSync("database.json"));
-  } else {
-     res.send({});
-  }
+app.post("/generate",(req,res)=>{
+ let token = crypto.randomBytes(16).toString("hex");
+ data[token] = [];
+ res.send({token});
 });
 
-app.listen(3000, ()=>{
-  console.log("Lost Phone Tracker running");
+app.post("/save/:token",(req,res)=>{
+ if(data[req.params.token]){
+   data[req.params.token].push(req.body);
+ }
+ res.send({ok:true});
 });
+
+app.get("/location/:token",(req,res)=>{
+ res.send(data[req.params.token] || []);
+});
+
+app.listen(3000,()=>console.log("Secure Lost Phone Tracker running"));
